359e2ee8f40bab4e7e94af40b5b65fe6326a3474
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-10-11 09:01:18 -0300

    Inicio do projeto.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 
     public static void main(String args[]) {
         
+        System.out.println("\nImplementacao iterativa:\n");
+        
         String entrada = "";
         if (args.length != 0) {
             entrada = args[0];
@@ -15,7 +17,7 @@ public class Principal {
         //Armazena o estado inicial
         int estado_inicial = 0;
         //Armazena o estado final
-        int estado_final = 2;
+        int estado_final = 3;
 
         //Armazena o estado atual
         int estado_atual = estado_inicial;
@@ -37,15 +39,33 @@ public class Principal {
             if (estado_atual == 0 && avaliar == 'a') {
                 estado_proximo = 1;
             }
-            // Estado 1 para 1
-            if (estado_atual == 1 && avaliar == 'b') {
+            // Estado 0 para 1
+            if (estado_atual == 0 && avaliar == 'b') {
                 estado_proximo = 1;
             }
-            // Estado 1 para 2 (Estado final)
+            // Estado 1 para 1
             if (estado_atual == 1 && avaliar == 'a') {
+                estado_proximo = 2;
+            }
+            // Estado 1 para 1
+            if (estado_atual == 1 && avaliar == 'b') {
+                estado_proximo = 2;
+            }
+            // Estado 2 para 3 (Estado final)
+            if (estado_atual == 2 && avaliar == 'a') {
                 //Se chegou no final da entrada
                 if (i + 1 == entrada.length()) {
-                    estado_proximo = 2;
+                    estado_proximo = 3;
+                } else {
+                    estado_proximo = -1;
+                    break;
+                }
+            }
+            // Estado 2 para 3 (Estado final)
+            if (estado_atual == 2 && avaliar == 'b') {
+                //Se chegou no final da entrada
+                if (i + 1 == entrada.length()) {
+                    estado_proximo = 3;
                 } else {
                     estado_proximo = -1;
                     break;
@@ -60,9 +80,9 @@ public class Principal {
 
         //Verifica se o estado atual é igual ao estado final
         if (estado_atual == estado_final) {
-            System.out.println("\nEntrada valida   :" + entrada);
+            System.out.println("Entrada valida   :" + entrada);
         } else {
-            System.out.println("\nEntrada invalida :" + entrada);
+            System.out.println("Entrada invalida :" + entrada);
         }
     }
 }
