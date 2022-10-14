d971609984e83e44a126204296d8a5ce516091bb
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-10-14 15:49:05 -0300

    Ajustes de comentários.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -3,10 +3,41 @@ import javax.swing.JOptionPane;
 
 public class Principal {
 
+    /**
+     * Estados do AFD.
+     */
+    enum Estado {
+        S0, S1, S2, S3
+    }
+
+    //Armazena o estado inicial
+    static Estado estado_inicial = Estado.S0;
+    //Armazena o estado final
+    static Estado estado_final = Estado.S3;
+    //Armazena o estado atual
+    static Estado estado_atual = estado_inicial;
+    //Armazena se a entrada é válida
+    static boolean valido = true;
+
+    //Indice da letra da entrada
+    static int i = 0;
+
+    //Retorna um caracter da entrada e avança o indice para o próximo
+    public static char proximoCaracter(String entrada) {
+        char retorno = 0;
+        if (i < entrada.length()) {
+            retorno = entrada.charAt(i);
+            i = i + 1;
+        } else {
+            retorno = 0;
+        }
+        return retorno;
+    }
+
     public static void main(String args[]) {
-        
+
         System.out.println("\nImplementacao iterativa:\n");
-        
+
         String entrada = "";
         if (args.length != 0) {
             entrada = args[0];
@@ -14,67 +45,72 @@ public class Principal {
             entrada = JOptionPane.showInputDialog("Digite uma palavra a ser avaliada:");
         }
 
-        //Armazena o estado inicial
-        int estado_inicial = 0;        
-        //Armazena o estado final
-        int estado_final = 3;
-        //Armazena o estado atual
-        int estado_atual = estado_inicial;
-        //Armazena o estado próximo
-        int estado_proximo = -1;
-        
-        //Indice da letra da entrada
-        int i = 0;
-
-        //Enquando não chegou no final da entrada
-        while (i < entrada.length())  {
+        //Enquando não chegou no final da entrada e seja válido.
+        while (i < entrada.length() && valido) {
 
             //Recupera um caracter
-            char avaliar = entrada.charAt(i);
+            char avaliar = proximoCaracter(entrada);
 
-            // Avaliando as combinações
-            // Estado 0 para 1
-            if (estado_atual == 0 && avaliar == 'a') {
-                estado_proximo = 1;
-            }
-            // Estado 0 para 1
-            if (estado_atual == 0 && avaliar == 'b') {
-                estado_proximo = 1;
-            }
-            // Estado 1 para 2
-            if (estado_atual == 1 && avaliar == 'a') {
-                estado_proximo = 2;
-            }
-            // Estado 1 para 2
-            if (estado_atual == 1 && avaliar == 'b') {
-                estado_proximo = 2;
-            }
-            // Estado 2 para 3 
-            if (estado_atual == 2 && avaliar == 'a') {                
-                estado_proximo = 3;                
-            }
-            // Estado 2 para 3 
-            if (estado_atual == 2 && avaliar == 'b') {
-                estado_proximo = 3;
-            }            
-            // Estado 3 (Estado final)
-            if (estado_atual == 3 ) {
-                
-                //Se tem caracteres a avaliar
-                if (i < entrada.length()) {
-                    estado_proximo = -1;                    
+            //Analise do estado
+            switch (estado_atual) {
+
+                case S0: {
+                    // Estado 0 para 1
+                    if (avaliar == 'a') {
+                        estado_atual = Estado.S1;
+                    } else {
+                        // Estado 0 para 1
+                        if (avaliar == 'b') {
+                            estado_atual = Estado.S1;
+                        } else {
+                            valido = false;
+                        }
+                    }
+                    break;
+                }
+
+                case S1: {
+                    // Estado 1 para 2
+                    if (avaliar == 'a') {
+                        estado_atual = Estado.S2;
+                    } else {
+                        if (avaliar == 'b') {
+                            estado_atual = Estado.S2;
+                        } else {
+                            valido = false;
+                        }
+                    }
+                    break;
+                }
+
+                case S2: {
+                    // Estado 2 para 3
+                    if (avaliar == 'a') {
+                        estado_atual = Estado.S3;
+                    } else {
+                        if (avaliar == 'b') {
+                            estado_atual = Estado.S3;
+                        } else {
+                            valido = false;
+                        }
+                    }
+                    break;
+                }
+
+                case S3: {
+                    // Estado 3 (final)
+
+                    //Se tem caracteres para ler torna inválida a palavra
+                    if (i >= entrada.length()) {
+                        valido = false;
+                    }
+                    break;
                 }
             }
-                                    
-            // Atualiza o estado atual
-            estado_atual = estado_proximo;
-            
-            //Incrementa o indice das letras da entrada	
-            i = i + 1;
         }
 
         //Verifica se o estado atual é igual ao estado final
-        if (estado_atual == estado_final) {
+        if ((estado_atual == estado_final) && (valido == true)) {
             System.out.println("Entrada valida   :" + entrada);
         } else {
             System.out.println("Entrada invalida :" + entrada);
